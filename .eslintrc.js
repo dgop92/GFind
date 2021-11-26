81ec9b85ff2d30ffcddb71e9536c06a960975068
dgop92 <inevaup@gmail.com>
2021-11-25 22:28:58 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": 0,
+    "react/jsx-props-no-spreading": 0,
+    "react/function-component-definition": 0,
+
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "max-len": [
+      2,
+      {
+        code: 88,
+      },
+    ],
+
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "func-names": "off",
+    "no-process-exit": "off",
+    "object-shorthand": "off",
+    "class-methods-use-this": "off",
+    "object-curly-newline": "off",
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p>hola</p>
+    </div>
+  );
+}
+
+export default App;
